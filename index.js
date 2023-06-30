3f92d76886229ab54cd53867da9e27c7be4e87d4
SteveMuiyuro <stevemuiyuro@gmail.com>
2023-06-30 12:44:10 +0300

    Add Morgan dependancy

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 var responseTime = require("response-time");
 app.use(express.json());
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(morgan(":method :url :status :response-time ms :body "));
+
+app.use(morgan("tiny"));
 
 persons = [
   {
